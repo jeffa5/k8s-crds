e2990d9110a5032d99b4262307165a237852393c
Andrew Jeffery <dev@jeffas.io>
2022-07-02 09:42:21 +0100

    Add derive feature for serde

--- k8s-crds-codegen/src/main.rs
+++ k8s-crds-codegen/src/main.rs
@@ -200,7 +200,7 @@ impl Crate {
         writeln!(
             ct,
             r#"k8s-openapi = {{ version = "0.15.0", features = ["v1_20"] }}
-serde = "1.0.137"
+serde = {{ version = "1.0.137", features = ["derive"] }}
 serde_yaml = "0.8.24""#
         )?;
         Ok(())
